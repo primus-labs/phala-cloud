0756cb6bbcde6d5edcd2afc765e3ccbd62148b53
Leechael Yim <yanleech@gmail.com>
2025-06-13 07:14:48 +0800

    feat: utils for deployment

--- js/src/utils/index.ts
+++ js/src/utils/index.ts
@@ -1,1 +1,3 @@
+export { encryptEnvVars } from "@phala/dstack-sdk/encrypt-env-vars";
+export { get_compose_hash, type AppCompose } from "./get_compose_hash";
 export { getErrorMessage } from "./get_error_message";
